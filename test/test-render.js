b63327060ea6936ba16280cd26570ac56ca1a0b4
Adrian Rossouw <adrian@developmentseed.org>
2010-12-17 08:51:10 +0200

    Fix table rendering and fix the render tests.

--- test/test-render.js
+++ test/test-render.js
@@ -1,111 +1,124 @@
-var forms = require('forms');
+var forms = require('../lib/forms'),
+    Form = forms.Form;
 
 
 var testWrap = function(tag){
-    exports[tag] = function(test){
-        var f = forms.create({fieldname: forms.fields.string()});
-        test.equals(
-            f.toHTML(forms.render[tag]),
-            '<' + tag + ' class="field">' +
-                '<label for="id_fieldname">Fieldname</label>' +
-                '<input type="text" name="fieldname" id="id_fieldname" />' +
-            '</' + tag + '>'
-        );
-        test.done();
-    };
+    exports[tag] = function(assert){
+        var form = Form.create({fields : {fieldname: forms.fields.string()}});
+        var f = new form();
+        f.init();
 
-    exports[tag + ' required'] = function(test){
-        var f = forms.create({
-            fieldname: forms.fields.string({required:true})
-        });
-        test.equals(
-            f.toHTML(forms.render[tag]),
-            '<' + tag + ' class="field required">' +
-                '<label for="id_fieldname">Fieldname</label>' +
-                '<input type="text" name="fieldname" id="id_fieldname" />' +
-            '</' + tag + '>'
-        );
-        test.done();
-    };
-
-    exports[tag + ' bound'] = function(test){
-        test.expect(1);
-        var f = forms.create({name: forms.fields.string()});
-        f.bind({name: 'val'}).validate(function(err, f){
-            test.equals(
+        assert.includes(
                 f.toHTML(forms.render[tag]),
                 '<' + tag + ' class="field">' +
-                    '<label for="id_name">Name</label>' +
-                    '<input type="text" name="name" id="id_name" value="val" />' +
+                '<label for="id_fieldname">Fieldname</label>' +
+                '<input type="text" name="fieldname" id="id_fieldname" class="text" />' +
                 '</' + tag + '>'
-            );
-        });
-        setTimeout(test.done, 25);
+                );
+
     };
 
-    exports[tag + ' bound error'] = function(test){
-        test.expect(1);
-        var f = forms.create({
+    exports[tag + ' required'] = function(assert){
+        var form = Form.create({fields: {
+            fieldname: forms.fields.string({required:true})
+        }});
+        var f = new form();
+        f.init();
+        assert.includes(
+                f.toHTML(forms.render[tag]),
+                '<' + tag + ' class="field required">' +
+                '<label for="id_fieldname">Fieldname <span class="required">*</span></label>' +
+                '<input type="text" name="fieldname" id="id_fieldname" class="text" />' +
+                '</' + tag + '>'
+                );
+
+    };
+
+    exports[tag + ' bound'] = function(assert){
+        var form = Form.create({fields: {name: forms.fields.string()}});
+        var f = new form();
+        f.instance = {name: 'val'};
+        f.init();
+
+        f.emit('validate', {}, {});
+
+        assert.includes(
+                f.toHTML(forms.render[tag]),
+                '<' + tag + ' class="field">' +
+                '<label for="id_name">Name</label>' +
+                '<input type="text" name="name" id="id_name" class="text" value="val" />' +
+                '</' + tag + '>'
+                );
+    };
+
+    exports[tag + ' bound error'] = function(assert){
+        var form = Form.create({fields: {
             field_name: forms.fields.string({
-                validators: [function(form, field, callback){
-                    callback('validation error');
-                }]
-            })
-        });
-        f.bind({field_name: 'val'}).validate(function(err, f){
-            test.equals(
+                            validators: [function(form, field, callback){
+                                            form.errors[field.key] = 'validation error';
+                                        }]
+                        })
+        }});
+        var f = new form();
+        f.instance = {field_name: 'val'};
+        f.init();
+        f.emit('validate', {}, {});
+        
+        assert.includes(
                 f.toHTML(forms.render[tag]),
                 '<' + tag + ' class="field error">' +
-                    '<p class="error_msg">validation error</p>' +
-                    '<label for="id_field_name">Field name</label>' +
-                    '<input type="text" name="field_name" id="id_field_name" ' +
-                    'value="val" />' +
+                '<p class="error_msg">validation error</p>' +
+                '<label for="id_field_name">Field name</label>' +
+                '<input type="text" name="field_name" id="id_field_name" class="text" ' +
+                'value="val" />' +
                 '</' + tag + '>'
-            );
-        });
-        setTimeout(test.done, 25);
+                );
     };
 
-    exports[tag + ' multipleCheckbox'] = function(test){
-        var f = forms.create({
+    exports[tag + ' multipleCheckbox'] = function(assert){
+        var form = Form.create({fields: {
             fieldname: forms.fields.string({
-                choices: {one: 'item one'},
+                           choices: {one: 'item one'},
                 widget: forms.widgets.multipleCheckbox()
-            })
-        });
-        test.equals(
-            f.toHTML(forms.render[tag]),
-            '<' + tag + ' class="field">' +
+                       })
+        }});
+        var f = new form();
+        f.init();
+        assert.includes(
+                f.toHTML(forms.render[tag]),
+                '<' + tag + ' class="field">' +
                 '<fieldset>' +
-                    '<legend>Fieldname</legend>' +
-                    '<input type="checkbox" name="fieldname" id="id_fieldname_one"'+
-                    ' value="one">' +
-                    '<label for="id_fieldname_one">item one</label>' +
+                '<legend>Fieldname</legend>' +
+                '<input type="checkbox" name="fieldname" id="id_fieldname_one"'+
+                ' class="checkbox multiple" value="one">' +
+                '<label for="id_fieldname_one">item one</label>' +
                 '</fieldset>' +
-            '</' + tag + '>'
-        );
-        test.done();
+                '</' + tag + '>'
+                );
+
     };
 
-    exports[tag + ' multipleRadio'] = function(test){
-        var f = forms.create({
+    exports[tag + ' multipleRadio'] = function(assert){
+        var form = Form.create({fields: {
             fieldname: forms.fields.string({
-                choices: {one: 'item one'},
+                           choices: {one: 'item one'},
                 widget: forms.widgets.multipleRadio()
-            })
-        });
-        test.equals(
-            f.toHTML(forms.render[tag]),
-            '<' + tag + ' class="field">' +
+                       })
+        }});
+        var f = new form();
+        f.init();
+        assert.includes(
+                f.toHTML(forms.render[tag]),
+                '<' + tag + ' class="field">' +
                 '<fieldset>' +
-                    '<legend>Fieldname</legend>' +
-                    '<input type="radio" name="fieldname" id="id_fieldname_one"'+
-                    ' value="one">' +
-                    '<label for="id_fieldname_one">item one</label>' +
+                '<legend>Fieldname</legend>' +
+                '<input type="radio" name="fieldname" id="id_fieldname_one"'+
+                ' class="radio multiple" value="one">' +
+                '<label for="id_fieldname_one">item one</label>' +
                 '</fieldset>' +
-            '</' + tag + '>'
-        );
-        test.done();
+                '</' + tag + '>'
+                );
+
     };
 
 };
@@ -114,75 +127,81 @@ testWrap('div');
 testWrap('p');
 testWrap('li');
 
-exports['table'] = function(test){
-    var f = forms.create({fieldname: forms.fields.string()});
-    test.equals(
-        f.toHTML(forms.render.table),
-        '<tr class="field">' +
-            '<th><label for="id_fieldname">Fieldname</label></th>' +
-            '<td>' +
-                '<input type="text" name="fieldname" id="id_fieldname" />' +
-            '</td>' +
-        '</tr>'
-    );
-    test.done();
-};
-
-exports['table required'] = function(test){
-    var f = forms.create({
-        fieldname: forms.fields.string({required:true})
-    });
-    test.equals(
-        f.toHTML(forms.render.table),
-        '<tr class="field required">' +
-            '<th><label for="id_fieldname">Fieldname</label></th>' +
-            '<td>' +
-                '<input type="text" name="fieldname" id="id_fieldname" />' +
-            '</td>' +
-        '</tr>'
-    );
-    test.done();
-};
-
-exports['table bound'] = function(test){
-    test.expect(1);
-    var f = forms.create({name: forms.fields.string()});
-    f.bind({name: 'val'}).validate(function(err, f){
-        test.equals(
+exports['table'] = function(assert){
+    var form = Form.create({fields: {fieldname: forms.fields.string()}});
+    var f = new form();
+    f.init();
+    assert.includes(
             f.toHTML(forms.render.table),
             '<tr class="field">' +
-                '<th><label for="id_name">Name</label></th>' +
-                '<td>' +
-                    '<input type="text" name="name" id="id_name"' +
-                    ' value="val" />' +
-                '</td>' +
+            '<th><label for="id_fieldname">Fieldname</label></th>' +
+            '<td>' +
+            '<input type="text" name="fieldname" id="id_fieldname" class="text" />' +
+            '</td>' +
             '</tr>'
-        );
-    });
-    setTimeout(test.done, 25);
+            );
+
 };
 
-exports['table bound error'] = function(test){
-    test.expect(1);
-    var f = forms.create({
+exports['table required'] = function(assert){
+    var form = Form.create({fields: {
+        fieldname: forms.fields.string({required:true})
+    }});
+    var f = new form();
+    f.init();
+    assert.includes(
+            f.toHTML(forms.render.table),
+            '<tr class="field required">' +
+            '<th><label for="id_fieldname">Fieldname <span class="required">*</span></label></th>' +
+            '<td>' +
+            '<input type="text" name="fieldname" id="id_fieldname" class="text" />' +
+            '</td>' +
+            '</tr>'
+            );
+
+};
+
+exports['table bound'] = function(assert){
+    var form = Form.create({fields: {name: forms.fields.string()}});
+    var f = new form();
+    f.instance = {name: 'val'};
+    f.init();
+        f.emit('validate', {}, {});
+    
+    assert.includes(
+            f.toHTML(forms.render.table),
+            '<tr class="field">' +
+            '<th><label for="id_name">Name</label></th>' +
+            '<td>' +
+            '<input type="text" name="name" id="id_name"' +
+            ' class="text" value="val" />' +
+            '</td>' +
+            '</tr>'
+            );
+};
+
+exports['table bound error'] = function(assert){
+    var form = Form.create({fields: {
         field_name: forms.fields.string({
-            validators: [function(form, field, callback){
-                callback('validation error');
-            }]
-        })
-    });
-    f.bind({field_name: 'val'}).validate(function(err, f){
-        test.equals(
+                        validators: [function(form, field, callback){
+                                        form.errors[field.key] = 'validation error';
+                                    }]
+                    })
+    }});
+    var f = new form();
+    f.instance = {field_name: 'val'};
+    f.init();
+        f.emit('validate', {}, {});
+    
+    assert.includes(
             f.toHTML(forms.render.table),
             '<tr class="field error">' +
-                '<th><label for="id_field_name">Field name</label></th>' +
-                '<td>' +
-                    '<p class="error_msg">validation error</p>' +
-                    '<input type="text" name="field_name"' +
-                    ' id="id_field_name" value="val" />' +
-                '</td>' +
+            '<th><label for="id_field_name">Field name</label></th>' +
+            '<td>' +
+            '<p class="error_msg">validation error</p>' +
+            '<input type="text" name="field_name"' +
+            ' id="id_field_name" class="text" value="val" />' +
+            '</td>' +
             '</tr>'
-        );
-    });
-    setTimeout(test.done, 25);
+            );
 };
